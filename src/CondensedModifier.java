b495f67a3b542f59f32b1d71c14e9a3f3ecb334e
davidr1702 <16690728+davidr1702@users.noreply.github.com>
2019-10-05 16:33:37 -0600

    Need testing

--- src/CondensedModifier.java
+++ src/CondensedModifier.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class CondensedModifier extends Modifier {
 
     public CondensedModifier(KNearestNeighborModel model) {
@@ -7,7 +9,37 @@ public class CondensedModifier extends Modifier {
 
     @Override
     void modifyTrainingSet(int numNeighbors) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    	ArrayList<Example> z= new ArrayList();
+    	ArrayList<Example> condensedTrainingSet= new ArrayList(model.trainingSet);
+    	Example xPrime=new Example(0.0, null);
+    	double distance=100.0;
+    	boolean change=true;
+    	
+    	while(change) {
+    		change=false;
+    		for (Example e : condensedTrainingSet) {
+    			model.trainingSet.remove(e);
+    			for (Example x: condensedTrainingSet) {
+    				model.trainingSet.remove(x);
+    				x.distanceFrom(e);
+    				double temp=x.distance;
+    				if (distance>temp) {
+    					distance=temp;
+    					xPrime=x;
+    				}
+    				model.trainingSet.add(x);
+    			}
+    			//double distance=e.distanceFrom(e);
+    			double actual= model.classify(xPrime, numNeighbors);
+                double predicted = model.classify(e, numNeighbors);
+                if (actual!=predicted) {
+                	z.add(xPrime);
+                	change=true;
+                }
+                model.trainingSet.add(e);
+            }
+    	}
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
